71bf9511af77fba6ab9473930403c2c51c7ffa71
Keya <keyagandhi251994@gmail.com>
2019-04-06 23:42:13 -0400

    Final commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
-   "URI" : "mongodb://localhost/asgn2",
-   // "URI" : "mongodb://thomas:a123456@ds060977.mlab.com:60977/comp308-w2019-lesson10b",
+  // "URI" : "mongodb://localhost/asgn2",
+    "URI" : "mongodb://keya:keya123@ds058508.mlab.com:58508/kasgn2",
     "secret": "SomeSecret"
 }
